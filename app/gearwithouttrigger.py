e38b28e9d0ab5c75740f65573d1cde9cd4ed8530
Pieter Cailliau <pieter.cailliau@gmail.com>
2020-06-11 17:54:06 +0100

    example gear without trigger

---
+++ app/gearwithouttrigger.py
@@ -0,0 +1,46 @@
+import redisAI
+import numpy as np
+
+def is_fraud(record):
+    # Retrieve tensors from keyspace
+    # Range query with limit 100. (Without limit it can return 100-150K results which reduce performance)
+    ref_data_keys = execute("ZRANGEBYSCORE", "references", record[1], record[2], "LIMIT", "0", "100")
+    # Set "_tensor" suffix for every returned key
+    keys = [x + "_tensor" for x in ref_data_keys]
+    # Append the new transaction tensor key
+    keys.append(record[3])
+    # Do mgetTensors from the keyspace
+    tensors = redisAI.mgetTensorsFromKeyspace(keys)
+
+    # Take the reference data tensors and the sample data
+    ref_data = tensors[:len(tensors) - 2]
+    new_sample = tensors[len(tensors) - 1]
+
+    # Create a new reference tensor out the of the reference data from the keyspace, with a torch script
+    scriptRunner = redisAI.createScriptRunner('concat_script', 'concat_tensors')
+    redisAI.scriptRunnerAddInputList(scriptRunner, ref_data)
+    redisAI.scriptRunnerAddOutput(scriptRunner)
+
+    # Run two models over the reference data and the transaction
+    ref_data = redisAI.scriptRunnerRun(scriptRunner)[0]
+    modelRunner = redisAI.createModelRunner('model_1')
+    redisAI.modelRunnerAddInput(modelRunner, 'transaction', new_sample)
+    redisAI.modelRunnerAddInput(modelRunner, 'reference', ref_data)
+    redisAI.modelRunnerAddOutput(modelRunner, 'output')
+    output_1 = redisAI.modelRunnerRun(modelRunner)[0]
+    modelRunner = redisAI.createModelRunner('model_2')
+    redisAI.modelRunnerAddInput(modelRunner, 'transaction', new_sample)
+    redisAI.modelRunnerAddInput(modelRunner, 'reference', ref_data)
+    redisAI.modelRunnerAddOutput(modelRunner, 'output')
+    output_2 = redisAI.modelRunnerRun(modelRunner)[0]
+
+    # Average the results with numpy and set in keyspace
+    shape = redisAI.tensorGetDims(output_1)
+    reply_ndarray_0 = np.frombuffer(redisAI.tensorGetDataAsBlob(output_1), dtype=np.float32).reshape(shape)
+    reply_ndarray_1 = np.frombuffer(redisAI.tensorGetDataAsBlob(output_2), dtype=np.float32).reshape(shape)
+    res = (reply_ndarray_0 + reply_ndarray_1) / 2.0
+    output = redisAI.createTensorFromBlob('FLOAT', res.shape, res.tobytes())
+    redisAI.setTensorInKey('model_result', output)
+    yield None
+
+GearsBuilder('ShardsIDReader').run(is_fraud([None,1,10,"transaction"]))
